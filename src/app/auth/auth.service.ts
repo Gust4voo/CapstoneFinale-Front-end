802e761a8e2174bb9cd1475c308eaa7e2be29bfe
Gustavo Oliveira <123893190+Gust4voo@users.noreply.github.com>
2023-09-28 00:18:54 +0200

    aggiornamento

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -6,71 +6,74 @@ import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class AuthService {
+    currentUser: any = null;
 
-  currentUser: any = null;
+    constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  login(username: string, password: string): Observable<any> {
-    const credentials = { username, password };
-    return this.http.post<any>('http://localhost:8080/api/auth/login', credentials)
-      .pipe(map(response => {
-        if (response.accessToken) {
-          console.log('Token:', response.accessToken);
-          localStorage.setItem('token', response.accessToken);
-        }
-        return response;
-      }));
-  }
-
-  register(username:String, name:String,  surname: string,
-    email: string,
-    password: string,
-    numeroTelefono: string): Observable<any> {
-      const newUser = { username, name, surname, email, password,};
-    return this.http.post<any>('http://localhost:8080/api/auth/register', newUser);
-  }
-
-
-  logout() {
-    localStorage.removeItem('token');
-    alert("CIAO,RIEFFETTUA IL LOGIN PER ACCEDERE")
-
-
-
-  }
-
-  isLoggedIn(): boolean {
-    return !!localStorage.getItem('token');
-  }
-
-  getToken(): string | null {
-    return localStorage.getItem('token');
-  }
-
-  getCurrentUser(): Observable<any> {
-    if (this.currentUser) {
-        return of(this.currentUser);
+    login(username: string, password: string): Observable<any> {
+        const credentials = { username, password };
+        return this.http
+            .post<any>('http://localhost:8080/api/auth/login', credentials)
+            .pipe(
+                map((response) => {
+                    if (response.accessToken) {
+                        console.log('Token:', response.accessToken);
+                        localStorage.setItem('token', response.accessToken);
+                    }
+                    return response;
+                })
+            );
     }
 
-    return this.http.get<any>('http://localhost:3001/users/current').pipe(
-        map(response => {
-            console.log("Risposta API:", response);
-            if (!response) {
-                throw new Error('Invalid API response');
-            }
-            this.currentUser = response;
-            return this.currentUser;
-        }),
-        catchError(error => {
-            console.error("Errore nell'API:", error);
-            return throwError(error);
-        })
-    );
+    register(
+        username: String,
+        name: String,
+        surname: string,
+        email: string,
+        password: string,
+        numeroTelefono: string
+    ): Observable<any> {
+        const newUser = { username, name, surname, email, password };
+        return this.http.post<any>(
+            'http://localhost:8080/api/auth/register',
+            newUser
+        );
+    }
 
+    logout() {
+        localStorage.removeItem('token');
+        alert('CIAO,RIEFFETTUA IL LOGIN PER ACCEDERE');
+    }
+
+    isLoggedIn(): boolean {
+        return !!localStorage.getItem('token');
+    }
+
+    getToken(): string | null {
+        return localStorage.getItem('token');
+    }
+
+    getCurrentUser(): Observable<any> {
+        if (this.currentUser) {
+            return of(this.currentUser);
+        }
+
+        return this.http.get<any>('http://localhost:3001/users/current').pipe(
+            map((response) => {
+                console.log('Risposta API:', response);
+                if (!response) {
+                    throw new Error('Invalid API response');
+                }
+                this.currentUser = response;
+                return this.currentUser;
+            }),
+            catchError((error) => {
+                console.error("Errore nell'API:", error);
+                return throwError(error);
+            })
+        );
     }
 }
-
--- src/app/auth/login2/login2.component.ts
+++ src/app/auth/login2/login2.component.ts
@@ -3,33 +3,31 @@ import { AuthService } from '../auth.service';
 import { Route, Router } from '@angular/router';
 
 @Component({
-  selector: 'app-login2',
-  templateUrl: './login2.component.html',
-  styleUrls: ['./login2.component.scss']
+    selector: 'app-login2',
+    templateUrl: './login2.component.html',
+    styleUrls: ['./login2.component.scss'],
 })
-
 export class Login2Component {
-    username:string="";
-    password:string=""
+    username: string = '';
+    password: string = '';
 
-    constructor(private authService:AuthService){}
+    constructor(private authService: AuthService) {}
 
     ngOnInit(): void {}
 
     login(): void {
         this.authService.login(this.username, this.password).subscribe(
-          (response) => {
-            // Login effettuato con successo
-            const token = this.authService.getToken();
-            console.log('Token:', token); // Verifica il token nella console
+            (response) => {
+                // Login effettuato con successo
+                const token = this.authService.getToken();
+                console.log('Token:', token); // Verifica il token nella console
 
-            //this.router.navigate(['/dashboard']);
-            console.log('Login effettuato:', response);
-          },
-          (error) => {
-
-            console.error('Errore di login:', error);
-          }
+                //this.router.navigate(['/dashboard']);
+                console.log('Login effettuato:', response);
+            },
+            (error) => {
+                console.error('Errore di login:', error);
+            }
         );
-      }
+    }
 }
